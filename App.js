e5ae96badf335f850a987fb87a461091f1f7b7e0
JennaLeysens <jennalicowley@gmail.com>
2020-06-26 13:48:28 +0200

    motion sensors: colours

--- App.js
+++ App.js
@@ -1,45 +1,7 @@
 import React from "react";
 import { View } from "react-native";
+import GameScreen from "./src/screens/GameScreen";
 
 export default function App() {
-  return (
-    <View style={{ height: "100%" }}>
-      <View
-        style={{
-          flex: 1,
-
-          alignSelf: "stretch",
-          width: "100%",
-          backgroundColor: "rebeccapurple",
-        }}
-      />
-      <View
-        style={{
-          flex: 2,
-
-          alignSelf: "stretch",
-          width: "100%",
-          backgroundColor: "crimson",
-        }}
-      />
-      <View
-        style={{
-          flex: 3,
-
-          alignSelf: "stretch",
-          width: "100%",
-          backgroundColor: "gold",
-        }}
-      />
-      <View
-        style={{
-          flex: 3,
-
-          alignSelf: "stretch",
-          width: "100%",
-          backgroundColor: "blue",
-        }}
-      />
-    </View>
-  );
+  return <GameScreen />;
 }
